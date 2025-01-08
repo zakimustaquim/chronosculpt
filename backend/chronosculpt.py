91a5b4a4bf08b7c9d4aed4aa89ceff24d0395e53
zm68526 <104472530+zakimustaquim@users.noreply.github.com>
2025-01-08 14:48:15 -0500

    2 additional routes
    /records/:user_id/ - GET, POST

--- backend/chronosculpt.py
+++ backend/chronosculpt.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify, g
+from flask import Flask, jsonify, g, request
+from datetime import datetime, timezone
 import psycopg2
 from psycopg2.extras import DictCursor
 from psycopg2.pool import SimpleConnectionPool
@@ -116,7 +117,7 @@ def get_habits_by_user_id(user_id):
         result = {'habits': 
                     [
                         {
-                            'id': habit[0], 
+                            'hid': habit[0], 
                             'uid': habit[1],
                             'name': habit[2],
                             'comments': habit[3],
@@ -130,17 +131,98 @@ def get_habits_by_user_id(user_id):
         return jsonify(result), 200
     
     except Exception as e:
-            return jsonify({'error' : str(e)}), 500
+        return jsonify({'error' : str(e)}), 500
 
-@app.route('/records/<user_id>/<int:timestamp>/')
-def get_records(user_id, timestamp):
+@app.route('/records/<user_id>/', methods=['GET'])
+def get_all_records(user_id):
     try:
-        # sql
-        return
-
+        g.cursor.execute('''
+            SELECT *
+            FROM records
+            WHERE uid = %s
+        ''', (user_id,))
+        records = g.cursor.fetchall()
+        print(records[0][2])
+        result = {'records': 
+                    [
+                        {
+                            'rid': record[0],
+                            'uid': record[1],
+                            'date': record[2],
+                            'q1notes': record[3],
+                            'q2notes': record[4],
+                            'q3notes': record[5],
+                            'q4notes': record[6],
+                            'entries': get_entries_by_rid(record[0])
+                        } 
+                        for record in records
+                    ]
+                }
+        return jsonify(result), 200
+    
     except Exception as e:
-            return jsonify({'error': str(e)}), 500
+        return jsonify({'error' : str(e)}), 500
+
+def get_entries_by_rid(rid):
+    g.cursor.execute('''
+            SELECT e.*, h.name as habit_name
+            FROM entries e
+            JOIN habits h ON e.hid = h.hid
+            WHERE e.rid = %s
+        ''', (rid,))
+    entries = g.cursor.fetchall()
+    result = [
+                {
+                    'eid': entry[0],
+                    'rid': entry[1],
+                    'hid': entry[2],
+                    'comments': entry[3],
+                    'done': entry[4],
+                    'quadrant': entry[5],
+                    'doneAt': entry[6],
+                    'split': entry[7],
+                    'habit_name': entry[8],
+                } 
+                for entry in entries
+            ]
+    return result
+
+@app.route('/records/<int:record_id>/', methods=["PUT"])
+def update_record(record_id):
+    return jsonify({'message': record_id})
+
+@app.route('/records/<user_id>/', methods=['POST'])
+def create_record(user_id):
+    try:
+        habits_response, status_code = get_habits_by_user_id(user_id)
+        habits = habits_response.get_json()['habits']
+
+        # insert row into records table
+        g.cursor.execute('''
+            INSERT INTO records (uid, q1notes, q2notes, q3notes, q4notes)
+            VALUES (%s, '', '', '', '')
+            RETURNING rid;
+        ''', (user_id,))
+        record_id = g.cursor.fetchone()[0]
         
+        for habit in habits:
+            g.cursor.execute('''
+                INSERT INTO entries (rid, hid, comments, quadrant)
+                VALUES (%s, %s, %s, %s);
+            ''', (record_id, habit['hid'], habit['comments'], habit['preferredQuadrant']))
+        
+        g.db.commit()
+        return jsonify({'id': record_id})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/habits/<user_id>/add/', methods=['POST'])
+def add_habit(user_id):
+    return jsonify({'message': user_id})
+
+@app.route('/habits/<int:habit_id>/', methods=['PUT'])
+def update_habit(habit_id):
+    return jsonify({'message': habit_id})
 
 @app.route('/', methods=['GET'])
 def hello_world():
